9ffda1b6c903319209b001a124ace03b28eed7ff
NAM HOÀNG QUỐC <20127566@student.hcmus.edu.vn>
2022-03-26 01:11:10 +0700

    update: route to /login

---
+++ client/src/components/app/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AccountConsumer, AccountProvider } from "../../stores/account";
+import routes from "../../routes";
+
+export default function App() {
+  return (
+    <Router>
+      <AccountProvider>
+        <AccountConsumer>
+          {(context) => {
+            if (
+              window.sessionStorage.getItem("token") ||
+              window.localStorage.getItem("token")
+            ) {
+              const token =
+                window.sessionStorage.getItem("token") ||
+                window.localStorage.getItem("token");
+              if (!context.account) {
+                const config = {
+                  headers: {
+                    authorization: token,
+                  },
+                };
+              }
+            } else {
+              if (window.location.pathname !== routes.login.path) {
+                window.location.href = routes.login.path;
+              }
+              return <div></div>;
+            }
+          }}
+        </AccountConsumer>
+      </AccountProvider>
+    </Router>
+  );
+}
